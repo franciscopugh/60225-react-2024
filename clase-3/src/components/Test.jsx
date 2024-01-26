13b6eaf7ba57f341f49534baa7152608bc641d67
Francisco Pugh <franciscopugh01@gmail.com>
2024-01-25 21:04:54 -0300

    Clase 3 B

--- clase-3/src/components/Test.jsx
+++ clase-3/src/components/Test.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react"
+
 export const Test = ({ mensaje }) => {
+    //Valor - Forma de modificarlo - Valor inicial
+    const [numero, setNumero] = useState(0)
+
     return (
         <div>
+            <button onClick={() => setNumero(numero + 1)}>Sumar boton</button>
+            <p>{numero}</p>
             <p>{mensaje}</p>
         </div>
     )
